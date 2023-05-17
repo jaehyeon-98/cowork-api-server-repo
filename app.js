83fb9f8f07daa8b57ce817ed6e81d978cad5e4ff
배재현 <jaehyeon@baejaehyeon-ui-MacBookPro.local>
2023-05-17 17:55:26 +0900

    메세지

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-let members = require("./members");
+const db = require("./models");
 
 const { Member } = db;
 
--- seeders/20230416084712-initialMembers.js
+++ seeders/20230416084712-initialMembers.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { query } = require("express");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
